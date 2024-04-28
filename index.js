03e49034d7596ec7aca94aea7182d552dd4cb26f
Abdallah Aloush <abdallahaloush6@gmail.com>
2024-04-28 11:18:04 +0300

    fix index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Sura = require('./sura.js');
 const Book = require('./book.js');
 const Suras = require('./suras.js');
 
-const suras = generateSuras(ayas);
-const newBook = new Book(suras);
+const suras = new Suras();
+const surasArr = suras.generateSuras()
+const newBook = new Book(surasArr);
 newBook.produceEpub();
